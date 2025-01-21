25f314b5cad32aa80ece16bf17a55485127d1719
sanjay7708 <sanjaysri1310@gmail.com>
2025-01-21 10:11:51 +0530

    allow host change

--- hostpro/settings.py
+++ hostpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)n1s@8h^ct(bc+915vo-okm*lh=eyhbml*01)5^xs^-(!2$u8c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['samplehost.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
